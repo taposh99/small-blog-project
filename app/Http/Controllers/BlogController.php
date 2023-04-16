d22abd0940de856a580daa8da974f16d0f4bb0a4
Saniatul Haque Taposh <32695871+taposh99@users.noreply.github.com>
2023-04-16 11:31:08 +0600

    first commit

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use toastr;
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function blog(Request $request)
+    {
+        // dd($request);
+     
+
+      
+        $blog = new Blog();
+        $blog->blog = $request->blog;
+     
+        $blog->save();
+        toastr()->success('Data has been saved successfully!');
+        return back();
+    
+        // return redirect('/all-blog')->with('message', 'Success');
+    }
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('frontEndAsset') }}/css/bootstrap.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <a href="" class="btn btn-primary">Blog List</a>
+                {{-- {{ route('all.blog') }} --}}
+            </div>
+
+
+        </div>
+        <br>
+        <br>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="{{ route('new.blog') }}" method="post" enctype="multipart/form-data">
+
+                            @csrf
+
+                            <label class="">Create Blog : </label>
+                            <input type="text" class="form-control" name="blog" required>
+                            <br>
+                            <div class="form-floating mb-3 mb-md-0">
+                            </div>
+                            <input type="submit" value="submit" class="form-control btn btn-danger">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+</body>
+
+</html>
