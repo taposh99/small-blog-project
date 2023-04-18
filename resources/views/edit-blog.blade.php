56d38bf5984436cb759decbbdece9ed09bce8b0f
Saniatul Haque Taposh <taposhmd2@gmail.com>
2023-04-18 23:38:02 +0600

    update

--- resources/views/edit-blog.blade.php
+++ resources/views/edit-blog.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Blog </title>
 
     <link rel="stylesheet" href="{{asset('frontEndAsset')}}/css/bootstrap.css">
 </head>
@@ -26,7 +26,7 @@
                         <form action="{{ route('update.blog') }}" method="post" enctype="multipart/form-data">
                             @csrf
                             <input type="hidden" value="{{ $blog->id }}" name="blog_id">
-                            <label class="">Update Blog : </label>
+                            <label class="">Update blog : </label>
                             <input type="text" class="form-control" value="{{ $blog->blog }}" name="blog"><br>
 
 
