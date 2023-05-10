399c465df1226affdcdba1d1d85ce035a60ebd52
Saniatul Haque Taposh <32695871+taposh99@users.noreply.github.com>
2023-05-10 10:10:36 +0600

    up

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Small blog site</title>
+    <title>Small blog project</title>
 
     <link rel="stylesheet" href="{{ asset('frontEndAsset') }}/css/bootstrap.css">
 </head>
@@ -32,7 +32,7 @@
 
                             @csrf
 
-                            <label class="">Create Blog : </label>
+                            <label class="">Create New Blog : </label>
                             <input type="text" class="form-control" name="blog" required>
                             <br>
                             <div class="form-floating mb-3 mb-md-0">
