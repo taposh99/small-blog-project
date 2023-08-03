518c2f1fe50d7b8b65e464b5f30831ad9ce2918a
Saniatul Haque Taposh <32695871+taposh99@users.noreply.github.com>
2023-08-03 15:06:34 +0600

    update dashboard design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Small blog project</title>
+    <title>Simple blog project</title>
 
     <link rel="stylesheet" href="{{ asset('frontEndAsset') }}/css/bootstrap.css">
 </head>
@@ -16,7 +16,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3">
-                <a href="{{ route('all.blog') }}" class="btn btn-primary"> Total blogs list </a>
+                <a href="{{ route('all.blog') }}" class="btn btn-primary"> blogs list </a>
              
             </div>
 
@@ -32,7 +32,7 @@
 
                             @csrf
 
-                            <label class="">Create Blogs : </label>
+                            <label class="">Create new Blogs site : </label>
                             <input type="text" class="form-control" name="blog" required>
                             <br>
                             <div class="form-floating mb-3 mb-md-0">
